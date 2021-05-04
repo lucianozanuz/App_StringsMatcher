076e8955a96f8166eca58fb9d54a15e1c9886bff
Luciano Zanuz <lzanuz@hotmail.com>
2021-05-04 15:00:47 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
+pip install --upgrade streamlit
+
 from collections import namedtuple
 import altair as alt
 import math
@@ -14,7 +16,7 @@ if st.button('Say hello'):
 else:
     st.write('Goodbye')
 
-with st.form:
+with st.form(key='my_form'):
     #st.header('This is some header.')
     st.subheader('Upload your Excel files')
     st.text('Upload excel files with only one column, even if you put multiple columns only the first one will be used')
