83157a6a9d3243bbe08780e5a23f5213d81a4048
Luciano Zanuz <lzanuz@hotmail.com>
2021-05-04 11:57:27 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,15 +5,11 @@ import pandas as pd
 import streamlit as st
 
 """
-# Welcome to Streamlit!
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-In the meantime, below is an example of what you can do with just a few lines of code:
+# Strings Matcher
+Texto explicativo em markdown
 """
 
-#threshold = st.slider("Similarity threshold", 0.7, 0, 1, help='Minimum similarity score to return, it goes from 0 to 1. If 1 it works exactly like a Left-join or Vlookup')
-#top_matches = st.st.number_input("Enter a number", value=1, help="Maximum number of matches to return. If 1 it shows only the best match, if greater than 1 it shows multiple matches")
+uploaded_file = st.file_uploader("Choose a file", help="ajuda") #Lookup list
 
 threshold = st.slider("Similarity threshold", 0.0, 1.0, 0.7, help="Minimum similarity score to return, it goes from 0 to 1. If 1 it works exactly like a Left-join or Vlookup")
 top_matches = st.number_input("Enter a number", value=1, help="Maximum number of matches to return. If 1 it shows only the best match, if greater than 1 it shows multiple matches")
