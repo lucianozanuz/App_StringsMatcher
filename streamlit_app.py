71430ecabb0753ef3c4ce65637f5e43ecc8b9eaa
Luciano Zanuz <lzanuz@hotmail.com>
2021-05-04 11:38:59 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,3 @@
-#threshold = st.slider("Similarity threshold", 0.7, 0, 1, help='Minimum similarity score to return, it goes from 0 to 1. If 1 it works exactly like a Left-join or Vlookup')
-#top_matches = st.st.number_input("Enter a number", value=1, help="Maximum number of matches to return. If 1 it shows only the best match, if greater than 1 it shows multiple matches")
-
 from collections import namedtuple
 import altair as alt
 import math
@@ -14,6 +11,9 @@ If you have any questions, checkout our [documentation](https://docs.streamlit.i
 forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
+
+threshold = st.slider("Similarity threshold", 0.7, 0, 1, help='Minimum similarity score to return, it goes from 0 to 1. If 1 it works exactly like a Left-join or Vlookup')
+top_matches = st.st.number_input("Enter a number", value=1, help="Maximum number of matches to return. If 1 it shows only the best match, if greater than 1 it shows multiple matches")
 
 
 with st.echo(code_location='below'):
