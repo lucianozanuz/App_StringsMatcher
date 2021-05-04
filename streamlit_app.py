1a04e0b35f50050f84548626534f0f073a8d5b45
Luciano Zanuz <lzanuz@hotmail.com>
2021-05-04 11:32:56 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,6 @@ In the meantime, below is an example of what you can do with just a few lines of
 """
 
 
-with st.echo(code_location='below'):
-    threshold = st.slider("Similarity threshold", 0.7, 0, 1, help='Minimum similarity score to return, it goes from 0 to 1. If 1 it works exactly like a Left-join or Vlookup')
-    top_matches = st.st.number_input('Enter a number', value=1, help='Maximum number of matches to return. If 1 it shows only the best match, if greater than 1 it shows multiple matches')
+#with st.echo(code_location='below'):
+threshold = st.slider("Similarity threshold", 0.7, 0, 1, help='Minimum similarity score to return, it goes from 0 to 1. If 1 it works exactly like a Left-join or Vlookup')
+top_matches = st.st.number_input("Enter a number", value=1, help="Maximum number of matches to return. If 1 it shows only the best match, if greater than 1 it shows multiple matches")
