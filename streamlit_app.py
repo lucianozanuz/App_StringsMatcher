33e5a60c4fce34c1a9a0f75475a0549bccd63b86
Luciano Zanuz <lzanuz@hotmail.com>
2021-05-04 11:43:24 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,8 @@ In the meantime, below is an example of what you can do with just a few lines of
 #threshold = st.slider("Similarity threshold", 0.7, 0, 1, help='Minimum similarity score to return, it goes from 0 to 1. If 1 it works exactly like a Left-join or Vlookup')
 #top_matches = st.st.number_input("Enter a number", value=1, help="Maximum number of matches to return. If 1 it shows only the best match, if greater than 1 it shows multiple matches")
 
-threshold = st.slider("Similarity threshold", 0.7, 0, 1)
+threshold = st.slider("Similarity threshold")
+top_matches = st.st.number_input("Enter a number")
 
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
