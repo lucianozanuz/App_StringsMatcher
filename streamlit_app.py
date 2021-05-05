3774061a6a8c994bbb727eba945a56dddb00317e
Luciano Zanuz <lzanuz@hotmail.com>
2021-05-05 01:19:32 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,15 @@ import base64
 import pandas as pd
 import streamlit as st
 
+def get_table_download_link(df):
+    """Generates a link allowing the data in a given panda dataframe to be downloaded
+    in:  dataframe
+    out: href string
+    """
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+    href = f'<a href="data:file/csv;base64,{b64}">Download csv file</a>'
+    
 """
 # Strings Matcher
 Texto explicativo em markdown
@@ -42,22 +51,14 @@ if st.button('Submit'):
         xlsx_out = model.write_excel(dtf_out)        
         st.markdown(get_table_download_link(df), unsafe_allow_html=True)
         dtf_out
+        
+        # When no file name is given, pandas returns the CSV as a string, nice.
+        df = pd.DataFrame(data, columns=["Col1", "Col2", "Col3"])
+        csv = dtf_out.to_csv(index=False)
+        b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+        href = f'<a href="data:file/csv;base64,{b64}">Download CSV File</a> (right-click and save as &lt;some_name&gt;.csv)'
+        st.markdown(href, unsafe_allow_html=True)
+
+        
 else:
     st.write('')
-
-def get_table_download_link(df):
-    """Generates a link allowing the data in a given panda dataframe to be downloaded
-    in:  dataframe
-    out: href string
-    """
-    csv = df.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
-    href = f'<a href="data:file/csv;base64,{b64}">Download csv file</a>'
-
-data = [(1, 2, 3)]
-# When no file name is given, pandas returns the CSV as a string, nice.
-df = pd.DataFrame(data, columns=["Col1", "Col2", "Col3"])
-csv = df.to_csv(index=False)
-b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
-href = f'<a href="data:file/csv;base64,{b64}">Download CSV File</a> (right-click and save as &lt;some_name&gt;.csv)'
-st.markdown(href, unsafe_allow_html=True)
