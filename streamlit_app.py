ad9be47fb546a37860e37c670ae1c2fc44023cd8
Luciano Zanuz <lzanuz@hotmail.com>
2021-05-05 01:35:50 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,8 +54,8 @@ if st.button('Submit'):
         dtf_out
         
         csv = dtf_out.to_csv(index=False)
-        b64 = base64.b64encode(xlsx_out.encode()).decode()  # some strings <-> bytes conversions necessary here
-        href = f'<a href="data:file/csv;base64,{b64}">Download CSV File</a> (right-click and save as &lt;some_name&gt;.csv)'
+        b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+        href = f'<a href="data:file/csv;base64,{b64}.csv">Download CSV File</a> (right-click and save as &lt;some_name&gt;.csv)'
         st.markdown(href, unsafe_allow_html=True)
         
 else:
